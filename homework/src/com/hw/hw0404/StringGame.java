a06d06e04718911708672573b1afdc215241dd58
summmmmin <129031836+summmmmin@users.noreply.github.com>
2023-04-04 22:54:07 +0900

    Update StringGame.java
    0404-1

--- homework/src/com/hw/hw0404/StringGame.java
+++ homework/src/com/hw/hw0404/StringGame.java
@@ -7,17 +7,19 @@ public class StringGame {
 	public String array[] = {"Orange", "Game", "Phone", "Smart"};
 	public int random;
 	public String answer;
-	public String checkchar;
-	public String checkstr;
+	public String checkchar = "";
+	
 	
 	public StringGame() {
-		this.random = (int) (Math.random() * 3);
+		this.random = (int) (Math.random() * 3+1)-1;
 		this.answer = array[random];
+		System.out.println("시작 random"+this.random+"answer"+this.answer);
 	}
 	
 	public void setCharData() {
 		// TODO Auto-generated method stub
 		System.out.println("현재까지 확인된 문자열의 문자들은 다음과 같습니다.");
+		System.out.println(this.checkchar);
 		System.out.print("입력>>");
 		char input = sc.next().charAt(0);
 		for(int i=0; i<this.answer.length(); i++) {
@@ -26,36 +28,47 @@ public class StringGame {
 				this.checkchar += this.answer.charAt(i);
 			}
 		}
-	}
-
-	public void showCharData() {
-		// TODO Auto-generated method stub
-		System.out.println("현재까지 확인된 문자열의 문자들은 다음과 같습니다.");
-		System.out.print("입력>>");
-		String input = sc.next();
-		for(int i=0; i<this.answer.length(); i++) {
-			if(input.charAt(i) == this.answer.charAt(i)) {
-				this.checkstr += "O";
-			}else {
-				this.checkstr += "X";
-			}
-		}
-		if(this.checkstr.contains("X")) {
-			System.out.println("입력된 문자열의 길이가 정답과 다릅니다.");
-		}else {
-			System.out.println("정답입니다.");
-		}
+		
+		
 	}
 
 	public void setStrData() {
 		// TODO Auto-generated method stub
 		System.out.println("현재까지 확인된 문자열의 문자들은 다음과 같습니다.");
+		System.out.println(this.checkchar);
+		String checkstr = "";
+		System.out.print("입력>>");
+		String input = sc.next();
+		if(input.length() == this.answer.length()) {
+			for(int i=0; i<this.answer.length(); i++) {
+				if(input.charAt(i) == this.answer.charAt(i)) {
+					checkstr += "O";
+				}else {
+					checkstr += "X";
+				}
+			}
+			if(checkstr.contains("X")) {
+				System.out.println(checkstr);
+			}else {
+				System.out.println("정답입니다.");
+			}			
+		}else {
+			System.out.println("입력된 문자열의 길이가 정답과 다릅니다.");
+		}
+	}
+
+	public void showCharData() {
+		// TODO Auto-generated method stub
+		System.out.println("현재까지 확인된 문자열의 문자들은 다음과 같습니다.");
+		System.out.println(this.checkchar);
 	}
 
 	public void init() {
 		// TODO Auto-generated method stub
-		this.random = (int) (Math.random() * 3);
+		this.random = (int) (Math.random() * 4+1)-1;
 		this.answer = array[random];
+		System.out.println("시작 random"+this.random+"answer"+this.answer);
+		this.checkchar="";
 	}
 
 }
