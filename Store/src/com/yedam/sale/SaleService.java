e3568e14c74977caa18c6fb623d4ab73b22ab955
user <user@DESKTOP-9HPM1AG>
2023-04-11 17:53:01 +0900

    0411-1

--- Store/src/com/yedam/sale/SaleService.java
+++ Store/src/com/yedam/sale/SaleService.java
@@ -3,12 +3,12 @@ package com.yedam.sale;
 import java.util.Scanner;
 
 public class SaleService {
+	Scanner sc = new Scanner(System.in);
 	//주문
 	//메뉴선택, 결제, 추소, 주문내역
 	
 	//메뉴선택
 	public void menuSelec() {
-		Scanner sc = new Scanner(System.in);
 		SaleDTO sale = new SaleDTO();
 		while(true) {
 			System.out.println("메뉴번호를 입력하세요(종료는 0)");
@@ -24,7 +24,13 @@ public class SaleService {
 			sale.setMenuId(menu);
 			sale.setSaledetailQty(qty);
 			int result = SaleDAO.getInstance().menuSelec(sale);
-		}
+		}		
+	}
+	
+	//주문하기(사용자)
+	public void saleAdd() {
+		SaleDTO sale = new SaleDTO();
+		
 		
 	}
 }
